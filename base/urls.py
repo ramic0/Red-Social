4f4e9a993b71a75557e5fe2faf77004605adbb3f
ramic0 <65687960+ramic0@users.noreply.github.com>
2023-02-23 21:50:26 -0500

    Creación de url de das, perfil, perfil str

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index, name='index' ),
+    path('das/', views.dashboard, name='dashboard'),
+    path('perfil/', views.perfil, name='perfil'),
+    path('perfil/<str:username>', views.perfil, name='perfil'),
 ]
