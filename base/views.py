c42351532758a8676623bf6f02aa063a8e6a993f
ramic0 <65687960+ramic0@users.noreply.github.com>
2023-02-23 21:49:45 -0500

    Creación de views de dashboard, perfil e index

--- base/views.py
+++ base/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Post,Perfil
 
 # Create your views here.
 def index(request):
     return render(request, 'base/index.html')
+
+def dashboard(request):
+    posts = Post.objects.all()
+    return render(request, 'base/dashboard.html', {'posts':posts})
+
+def perfil(request):
+    return render(request, 'base/perfil.html', {})
