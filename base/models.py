56ebb5904a40e38cb14a5557116f5fd0b8ed30ec
ramic0 <65687960+ramic0@users.noreply.github.com>
2023-02-23 21:52:44 -0500

    Creación de model de perfil y post

--- base/models.py
+++ base/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+class Perfil(models.Model):
+    usernames = models.CharField(max_length=10, blank=False, null=False)
+    name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(max_length=100, blank=False, null=False)
+    bio = models.TextField()
+    avatar = models.ImageField(default='image/usuario.png')
 
-# Create your models here.
+    def __str__(self):
+        return self.usernames
+
+class Post(models.Model):
+    user = models.ForeignKey(Perfil, on_delete=models.CASCADE)
+    time = models.DateTimeField(default=timezone.now)
+    content = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.content
+
