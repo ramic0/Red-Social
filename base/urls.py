7056c62c66b78373fa627b49e5d8203bcf7fdb84
ramic0 <65687960+ramic0@users.noreply.github.com>
2023-02-25 18:31:08 -0500

    Url para visualizar los post que el usuario a creado y perfil que posteriormente se va a utilizar la funcion

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index' ),
-    path('das/', views.dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('perfil/', views.perfil, name='perfil'),
-    path('perfil/<str:username>', views.perfil, name='perfil'),
+    #path('perfil/<str:username>', views.perfil, name='perfil'),
 ]
