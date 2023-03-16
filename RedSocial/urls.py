14c5616071797669b2c685e5d4d3a308a20c52d5
ramic0 <65687960+ramic0@users.noreply.github.com>
2023-03-15 19:44:29 -0500

    agregar static settings para archivos estaticos y eliminando accounts

--- RedSocial/urls.py
+++ RedSocial/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls')),
     path('', include('base.urls')),
-]
+    path('accounts/', include('accounts.urls')),
+    path('post/', include('post.urls')),
+    path('', include('base.urls')),
+    path('', include('api.urls')),
+] 
+
+urlpatterns = urlpatterns+static(settings.MEDIA_URL,
+document_root=settings.MEDIA_ROOT)
+
