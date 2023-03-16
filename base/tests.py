0ab7391933325a97c251a27a035e2606cb01ed4a
ramic0 <65687960+ramic0@users.noreply.github.com>
2023-03-15 20:27:27 -0500

    Prueba de test para la longotide context

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Perfil
 
 # Create your tests here.
+
+#class AuthorListViewTest(TestCase):
+#    def test_view_url_exists_at_desired_location(self):
+#        resp = self.client.get('/perfiles/')
+#        self.assertEqual(resp.status_code, 200)
+
+class PerfilTestCase(TestCase):
+    def setUp(self):
+        Perfil.objects.create(name="jefferson", email="jeff@gamil.com", bio="software")
+
+    def lenght(self):
+       post = Perfil.objects.get(name='jefferson').post_set-all()[0]
+       max_length = post._meta.get_field('content').max_length
+       self.assertEquals(max_length,600)
+
+    
