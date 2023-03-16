eaf2993b0f25812ca27471bcee513045de0aa9ad
ramic0 <65687960+ramic0@users.noreply.github.com>
2023-03-15 19:59:41 -0500

    Crear funciones  de like

---
+++ post/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+    
