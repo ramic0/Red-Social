99e41b347659f1782a78ef466f549e085867ef7d
ramic0 <65687960+ramic0@users.noreply.github.com>
2023-03-15 19:32:34 -0500

    eliminación de view signupView en url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
-from .views import SignUpView
+from . import views
+from django.contrib.auth.views import LoginView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup' ),
+    path('', views.muro, name='muro' ),
+    path('signup/', views.signup, name='signup' ),
+    path('login/', LoginView.as_view(), name='login' ),
+    path('perfil/', views.ver_perfil, name='ver_perfil' ),
+    path('profile/<str:username>/', views.profile, name='profile'),
+    path('logout/', views.signout, name='logout' ),
+    path('signin/', views.signin, name='signin' ),
+    path('settings/', views.settings, name='settings' ),
+    path('upload', views.upload, name='upload'),
+    path('follow/<str:username>/', views.follow, name='follow'),
+    path('unfollow/<str:username>/', views.unfollow, name='unfollow'),
 ]
