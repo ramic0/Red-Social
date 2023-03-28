80f5e83c9a42ed7e576a834441db3afe47f52674
ramic0 <65687960+ramic0@users.noreply.github.com>
2023-03-28 00:42:45 -0500

    Agragar FllowesCounts, crear una funcion de follow

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Post,Perfil
+from .models import Post,Perfil, FollowersCount
 from .forms import PostForm
 from django.utils import timezone
 from django.contrib.auth import authenticate, login
@@ -32,6 +32,22 @@ def recuperar_perfil(request,id):
         'perfil': perfil
     }
     return render(request, 'base/perfil.html', context)
+
+@login_required 
+def follow(request):
+    if request.method== 'POST':
+        follower = request.POST['follower']
+        user = request.POST['user']
+        if FollowersCount.objects.filter(follower=follower, user=user).first():
+            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+            return redirect('accounts/profile/'+ user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower, user=user)
+            new_follower.save()
+            return redirect('accounts/profile/'+ user)
+    else:
+        return redirect('home')
 
 
 
