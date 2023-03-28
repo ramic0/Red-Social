3ed87a09dd175dbca336b0818a0d1d64b7bae93d
ramic0 <65687960+ramic0@users.noreply.github.com>
2023-03-28 00:40:03 -0500

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,11 +9,9 @@ urlpatterns = [
     path('signup/', views.signup, name='signup' ),
     path('login/', LoginView.as_view(), name='login' ),
     path('perfil/', views.ver_perfil, name='ver_perfil' ),
-    path('profile/<str:username>/', views.profile, name='profile'),
+    path('profile/<str:pk>/', views.profile, name='profile'),
     path('logout/', views.signout, name='logout' ),
     path('signin/', views.signin, name='signin' ),
     path('settings/', views.settings, name='settings' ),
     path('upload', views.upload, name='upload'),
-    path('follow/<str:username>/', views.follow, name='follow'),
-    path('unfollow/<str:username>/', views.unfollow, name='unfollow'),
 ]
