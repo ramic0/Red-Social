bb11c31be3374cc2f3705eff4e524a1a18bdd47e
ramic0 <65687960+ramic0@users.noreply.github.com>
2023-03-28 00:41:16 -0500

    Eliminar linea 2 y 5 para colocar el nuevo import

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Perfil, Post
+from .models import Perfil, Post, FollowersCount
 
 admin.site.register(Perfil)
 admin.site.register(Post)
+admin.site.register(FollowersCount)
